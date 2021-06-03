0da295e70e745e6df50d41f08340e4777c4d4e46
kursatbakis <kursattek@gmail.com>
2021-06-03 17:25:25 +0300

    Initial Commit

---
+++ LoodosChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoodosChallenge
+//
+//  Created by Sociable on 3.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
